d5dced88adaa2b69758c790517625261141febc5
priyankara3cs <priyankara@3cs.solutions>
2024-08-04 14:44:28 +0530

    test 2

--- src/templates/home/HomePageNew.jsx
+++ src/templates/home/HomePageNew.jsx
@@ -1,26 +1,59 @@
 import React from "react";
+import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 import "./new.css";
 
 const Banner = () => {
   return (
-    <>
-      {/* section 1 */}
-      <div className="background-container">
-        <div className="car">
-          <img
-            src="/images/car.webp"
-            alt="Car"
-            style={{ width: "300px", height: "300px" }}
-          />
-        </div>
-        <div className="main-topic">
-          <h1>Main Topic</h1>
-        </div>
-      </div>
+    <div>
+      <Parallax pages={2}>
+        {/* Background Layer */}
+        <div className="background-container" />
 
-      {/* section 2 */}
-      <div style={{ height: "100px", backgroundColor: "#000" }}></div>
-    </>
+        {/* Car Image Layer */}
+        <ParallaxLayer
+          offset={0}
+          speed={0.5}
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <div className="car">
+            <img
+              src="/images/car.webp"
+              alt="Car"
+              style={{ width: "300px", height: "300px" }}
+            />
+          </div>
+        </ParallaxLayer>
+
+        {/* Main Topic Text Layer */}
+        <ParallaxLayer
+          offset={0}
+          speed={1}
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            color: "white",
+            fontSize: "2rem",
+          }}
+        >
+          <div className="main-topic">
+            <h1>Main Topic</h1>
+            <h2>sub Topic</h2>
+          </div>
+        </ParallaxLayer>
+      </Parallax>
+      {/* Section 2 */}
+      <p>
+        Welcome to the Parallax Scrolling Website project! This project
+        showcases an interactive and engaging website with a parallax scrolling
+        effect. The website is designed using HTML5, CSS3, and JavaScript to
+        create an immersive experience for users.{" "}
+      </p>
+    </div>
   );
 };
 
