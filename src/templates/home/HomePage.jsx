e79e9ddd1ff979da9579956a1c397440bb1aa55e
priyankara3cs <priyankara@3cs.solutions>
2024-08-04 10:51:04 +0530

    update

--- src/templates/home/HomePage.jsx
+++ src/templates/home/HomePage.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import React from "react";
+import { Parallax } from "react-parallax";
+import "./ParallaxBanner.css";
 
-export default function HomePage() {
+const ParallaxBanner = () => {
   return (
-    <div>HomePage</div>
-  )
-}
+    <div className="parallax-container">
+      <Parallax bgImage="/images/background2.jpeg" strength={200}>
+        <div style={{ height: 800 }}></div>
+      </Parallax>
+      <div className="parallax-layer planet">
+        <img src="/images/planet.png" alt="Planet" strength={150} />
+      </div>
+      <div className="parallax-layer land">
+        <img src="/images/land.png" alt="Land" strength={250} />
+      </div>
+      <div className="parallax-layer people">
+        <img src="/images/people.png" alt="People" strength={250} />
+      </div>
+      <div className="parallax-layer topic">
+        <h1>Your Topic Here</h1>
+      </div>
+    </div>
+  );
+};
+
+export default ParallaxBanner;
