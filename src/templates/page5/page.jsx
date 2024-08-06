c0a0824014a54f6d4a56d503bf069ce65443ecd9
priyankara3cs <priyankara@3cs.solutions>
2024-08-06 16:48:11 +0530

    update

--- src/templates/page5/page.jsx
+++ src/templates/page5/page.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import "../../components/homevideo.css";
 import { Parallax, ParallaxProvider } from "react-scroll-parallax";
 import Cursor from "../../components/Cursor";
+import "./slider.css";
 
 const ContractManufacturing = () => {
   const [scaling] = useState(false);
@@ -12,44 +13,46 @@ const ContractManufacturing = () => {
 
   return (
     <ParallaxProvider>
-      <div style={{ backgroundColor: "#121718" }}>
-        <div style={{ position: "relative" }}>
-          <div>
-            <Cursor scaling={scaling} />
-          </div>
-          <div className="background-video" />
-          <div className="top-font-block">
-            <div className="planet">
-              <img src="/images/planet.png" alt="planet" data-aos="fade-up" />
+      <div className="custom-slider-wrapper">
+        <div style={{ backgroundColor: "#121718" }}>
+          <div style={{ position: "relative" }}>
+            <div>
+              <Cursor scaling={scaling} />
             </div>
-          </div>
-          <div className="top-font-block">
-            <div className="land">
-              <img src="/images/land.png" alt="land" />
-            </div>
-            <div className="people">
-              <img src="/images/people.png" alt="land" />
-            </div>
-          </div>
-          <div className="top-font-block">
-            <Parallax speed={10}>
-              <div style={{ color: "#fff" }}>
-                <h1
-                  style={{
-                    animation: "slider-left 1s",
-                  }}
-                >
-                  Main Topic
-                </h1>
-                <h2
-                  style={{
-                    animation: "slider-right 1s",
-                  }}
-                >
-                  Sub Topic
-                </h2>
+            <div className="background-video" />
+            <div className="top-font-block">
+              <div className="planet">
+                <img src="/images/planet.png" alt="planet" data-aos="fade-up" />
               </div>
-            </Parallax>
+            </div>
+            <div className="top-font-block">
+              <div className="land">
+                <img src="/images/land.png" alt="land" />
+              </div>
+              <div className="people">
+                <img src="/images/people.png" alt="land" />
+              </div>
+            </div>
+            <div className="top-font-block">
+              <Parallax speed={10}>
+                <div style={{ color: "#fff" }}>
+                  <h1
+                    style={{
+                      animation: "slider-left 1s",
+                    }}
+                  >
+                    Main Topic
+                  </h1>
+                  <h2
+                    style={{
+                      animation: "slider-right 1s",
+                    }}
+                  >
+                    Sub Topic
+                  </h2>
+                </div>
+              </Parallax>
+            </div>
           </div>
         </div>
       </div>
